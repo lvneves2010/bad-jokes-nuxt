fff8cf5f9d17bf65c62b905e2c50ac65c612497f
Leonardo <lvneves2010@gmail.com>
2020-05-08 13:18:52 -0300

    initial commit

---
+++ pages/about.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <h1>About Bad Jokes</h1>
+      <p>This is an app to show corny jokes</p>
+  </div>
+</template>
+
+<script>
+export default {
+    head() {
+        return {
+            title: 'About the app',
+            meta: [
+                {
+                    hid: 'about',
+                    name: 'description of the app',
+                    content: 'Best place for corny dad jokes'
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
